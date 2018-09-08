ac921dc3ebbc292474729deeecbd7552ad0141c7
Oscar Valente <oscar.valente@blip.pt>
2018-09-08 09:34:26 +0100

    update readme with todo features and bug fixes

--- README.md
+++ README.md
@@ -9,31 +9,36 @@
     * add timestamp to tracked item object ✅
  * highlight current tab mouseover elements ✅
  * price tracking ✅
- * price tag element saving (per web-app) - auto-save button `(wip)`
+    * stop following when price decreases (notification)
+    * price increase alert
+        * user review possibility
+ * price tag element saving (per web-app) - auto-save button ✅
     * enabled when item from same domain is already being tracked ✅
     * change auto-save button color ✅
     * price tracking element selection saving (same url pages) ✅
     * when enabled, highlight price element on hover (w/ same color) ✅
     * spa url change defaultCursor bug ✅
- * extension icon displaying page is being tracked
  * tracked items management panel `(wip)`
     * basic ui ✅
     * delete ✅
-    * csp policy ✅
-    * watched/not_found items status and icon **(next)**
- * csp inline execution manifest review ✅
- * keep in items object in state
+    * execution manifest review ✅
+    * watched/not_found & price increase items statuses (with matching icon) ❗
+ * extension icon displaying page is being tracked
+ * keep tracked items object in state
  * saved money info (calc icon)
  * price tracking suggestion
     * intelligent price detection
     * extension icon displaying page has prices (change extension style)
     * price tracking suggestion preference toggling
  * price history graph
+    * migrate persisted state to IndexedDB ❕
+    * graph button on management panel
+    * new view
  * price alert setting
     * percentage threshold
     * value threshold
- * price increase alert
-    * user review possibility
+    * has threshold defined status
+    * price decrease status but not yet reach threshold (and icon)
  * extension user tour
  * minor improvements to the ui (styling minor issues)
  * style ui (major)
--- background.js
+++ background.js
@@ -104,67 +104,70 @@ function setSiblingTargetHighlight(state, isSiblingTargetHighlighted, originalBa
 function checkForPriceChanges() {
     State = resetNotifications(State);
     chrome.storage.sync.get(null, items => {
-        const domains = Object.keys(items);
-        chrome.storage.sync.get(domains, trackedItems => {
-            for (domain of domains) {
-                const domainItems = JSON.parse(trackedItems[domain]);
+            const domains = Object.keys(items);
+            chrome.storage.sync.get(domains, trackedItems => {
+                for (domain of domains) {
+                    const domainItems = JSON.parse(trackedItems[domain]);
 
-                for (let url in domainItems) {
-                    const request = new XMLHttpRequest();
-                    // TODO: const oldPrice
-                    let oldPrice = domainItems[url].price;
+                    for (let url in domainItems) {
+                        if (domainItems.hasOwnProperty(url)) {
+                            const request = new XMLHttpRequest();
+                            const oldPrice = domainItems[url].price;
 
-                    request.onload = function () {
-                        const template = createHTMLTemplate(this.response);
-                        try {
-                            let newPrice = null;
-                            const innerText = template.querySelector(domainItems[url].selection).innerText;
-                            if (innerText) {
-                                const innerTextMatch = innerText.match(/((?:\d+[.,])?\d+(?:[.,]\d+)?)/);
-                                if (innerTextMatch) {
-                                    [, newPrice] = innerTextMatch;
+                            request.onload = function () {
+                                const template = createHTMLTemplate(this.response);
+                                try {
+                                    let newPrice = null;
+                                    const innerText = template.querySelector(domainItems[url].selection).innerText;
+                                    if (innerText) {
+                                        const innerTextMatch = innerText.match(/((?:\d+[.,])?\d+(?:[.,]\d+)?)/);
+                                        if (innerTextMatch) {
+                                            [, newPrice] = innerTextMatch;
 
-                                    newPrice = toPrice(newPrice);
+                                            newPrice = toPrice(newPrice);
 
-                                    if (!oldPrice) {
-                                        chrome.storage.sync.set({[domain]: JSON.stringify(domainItems)}, () => {
-                                            const notificationId = `TRACK.PRICE_FIXED-${State.notificationsCounter}`;
-                                            createNotification(notificationId, ICONS.PRICE_FIX, "Fixed price",
-                                                `Ermm.. We've just fixed a wrongly set price to ${newPrice}`, url, url);
-                                        });
-                                    } else if (newPrice < oldPrice) {
-                                        domainItems[url].price = newPrice;
-                                        chrome.storage.sync.set({[domain]: JSON.stringify(domainItems)}, () => {
-                                            // TODO: sendResponse("done"); // foi actualizado ou não
-                                            const notificationId = `TRACK.PRICE_UPDATE-${State.notificationsCounter}`;
-                                            createNotification(notificationId, ICONS.PRICE_UPDATE, "Lower price!!",
-                                                `Lower price ${newPrice} (previous ${oldPrice})`, url, url);
-                                        });
+                                            if (!oldPrice) {
+                                                chrome.storage.sync.set({[domain]: JSON.stringify(domainItems)}, () => {
+                                                    const notificationId = `TRACK.PRICE_FIXED-${State.notificationsCounter}`;
+                                                    createNotification(notificationId, ICONS.PRICE_FIX, "Fixed price",
+                                                        `Ermm.. We've just fixed a wrongly set price to ${newPrice}`, url, url);
+                                                });
+                                            } else if (newPrice < oldPrice) {
+                                                domainItems[url].price = newPrice;
+                                                chrome.storage.sync.set({[domain]: JSON.stringify(domainItems)}, () => {
+                                                    // TODO: sendResponse("done"); // foi actualizado ou não
+                                                    const notificationId = `TRACK.PRICE_UPDATE-${State.notificationsCounter}`;
+                                                    createNotification(notificationId, ICONS.PRICE_UPDATE, "Lower price!!",
+                                                        `Lower price ${newPrice} (previous ${oldPrice})`, url, url);
+                                                });
+                                            }
+                                        }
                                     }
+
+                                    if (domainItems[url].price && !newPrice) {
+                                        const notificationId = `TRACK.PRICE_NOT_FOUND-${State.notificationsCounter}`;
+                                        const previousPrice = oldPrice ? ` (previous ${oldPrice})` : "";
+                                        createNotification(notificationId, ICONS.PRICE_NOT_FOUND, "Price gone",
+                                            `Price tag no longer found${previousPrice}`, url, url);
+                                    }
+                                } catch (e) {
+                                    console.warn(`Invalid price selection element in\n${url}:\t"${domainItems[url].selection}"`);
+                                    const notificationId = `TRACK.PRICE_NOT_FOUND-${State.notificationsCounter}`;
+                                    const previousPrice = oldPrice ? ` (previous ${oldPrice})` : "";
+                                    createNotification(notificationId, ICONS.PRICE_NOT_FOUND, "Price gone",
+                                        `Price tag no longer found${previousPrice}`, url, url);
                                 }
-                            }
+                            };
 
-                            if (domainItems[url].price && !newPrice) {
-                                const notificationId = `TRACK.PRICE_NOT_FOUND-${State.notificationsCounter}`;
-                                const previousPrice = oldPrice ? ` (previous ${oldPrice})` : "";
-                                createNotification(notificationId, ICONS.PRICE_NOT_FOUND, "Price gone",
-                                    `Price tag no longer found${previousPrice}`, url, url);
-                            }
-                        } catch (e) {
-                            console.warn(`Invalid price selection element in\n${url}:\t"${domainItems[url].selection}"`);
-                            const notificationId = `TRACK.PRICE_NOT_FOUND-${State.notificationsCounter}`;
-                            const previousPrice = oldPrice ? ` (previous ${oldPrice})` : "";
-                            createNotification(notificationId, ICONS.PRICE_NOT_FOUND, "Price gone",
-                                `Price tag no longer found${previousPrice}`, url, url);
+                            request.open("GET", url);
+                            request.send();
                         }
-                    };
-
-                    request.open("GET", url);
-                    request.send();
+                    }
                 }
-            }
-        });
-    });
+            });
+        }
+    )
+    ;
 }
 
 function getTrackedItems(callback) {
