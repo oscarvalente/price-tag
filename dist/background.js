560d7851b8651e4127ccdf81b49ca2fbeb2ccf72
Oscar Valente <oscar.valente@blip.pt>
2018-11-02 17:27:27 +0000

    temporary fix lets see

--- dist/background.js
+++ dist/background.js
@@ -10018,19 +10018,32 @@
     return fromEventPattern(addHistoryStateUpdatedHandler);
   }
 
+  function filterNavigation(getState$, navigation) {
+    const {
+      url
+    } = navigation;
+    return getState$().pipe(map(({
+      browserURL
+    }) => browserURL), filter(browserURL => url !== undefined && url !== browserURL), map(() => navigation));
+  }
+
   function onCompletedTab() {
-    const onNavigationCompleted$ = onCompleted().pipe(filter(frameId => frameId === 0));
-    const onCompletedTab$ = combineLatest(onNavigationCompleted$, onHistoryStateUpdated());
     const getActiveTab$ = queryActiveTab().pipe(filter(tabs => tabs.length > 0));
-    return onCompletedTab$.pipe(switchMap(() => getActiveTab$), map(([{
+    const onNavigationCompleted$ = onCompleted().pipe(filter(frameId => frameId === 0), switchMap(() => getActiveTab$), map(([{
       id,
       url
     }]) => ({
       id,
       url
-    })), filter(({
+    })));
+    const onNavigationHistoryStateUpdated$ = onHistoryStateUpdated().pipe(switchMap(() => getActiveTab$), map(([{
+      id,
       url
-    }) => url !== undefined));
+    }]) => ({
+      id,
+      url
+    })), switchMap(navigation => filterNavigation(StateManager.getState$, navigation)));
+    return combineLatest(onNavigationCompleted$, onNavigationHistoryStateUpdated$);
   }
 
   function addNotificationsButtonClickedHandler(handler) {
--- src/core/events/completed-tab.js
+++ src/core/events/completed-tab.js
@@ -3,21 +3,35 @@ import {filter, map, switchMap} from "rxjs/operators";
 import queryActiveTab$ from "./internal/query-active-tab";
 import onCompleted$ from "./internal/completed";
 import onHistoryStateUpdated$ from "./internal/history-state-updated";
+import StateManager from "../state-manager";
+
+function filterNavigation(getState$, navigation) {
+    const {url} = navigation;
+    
+    return getState$().pipe(
+        map(({browserURL}) => browserURL),
+        filter(browserURL => url !== undefined && url !== browserURL),
+        map(() => navigation)
+    );
+}
 
 function onCompletedTab() {
-    const onNavigationCompleted$ = onCompleted$()
-        .pipe(
-            filter(frameId => frameId === 0),
-        );
-    const onCompletedTab$ = combineLatest(onNavigationCompleted$, onHistoryStateUpdated$());
     const getActiveTab$ = queryActiveTab$()
         .pipe(filter(tabs => tabs.length > 0));
 
-    return onCompletedTab$.pipe(
+    const onNavigationCompleted$ = onCompleted$()
+        .pipe(
+            filter(frameId => frameId === 0),
+            switchMap(() => getActiveTab$),
+            map(([{id, url}]) => ({id, url}))
+        );
+    const onNavigationHistoryStateUpdated$ = onHistoryStateUpdated$().pipe(
         switchMap(() => getActiveTab$),
         map(([{id, url}]) => ({id, url})),
-        filter(({url}) => url !== undefined)
+        switchMap(navigation => filterNavigation(StateManager.getState$, navigation))
     );
+
+    return combineLatest(onNavigationCompleted$, onNavigationHistoryStateUpdated$);
 }
 
 export default onCompletedTab;
